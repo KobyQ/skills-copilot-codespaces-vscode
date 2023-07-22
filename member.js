07714a6bdde5030484f9376b6ba6136132fd3d2e
David Quagraine <kobequagraine@gmail.com>
2023-07-22 19:57:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,26 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var memberText = document.getElementById("memberText");
+    var skillsText = document.getElementById("skillsText");
+    var projectsText = document.getElementById("projectsText");
+    var contactText = document.getElementById("contactText");
+    var memberImg = document.getElementById("memberImg");
+    var skillsImg = document.getElementById("skillsImg");
+    var projectsImg = document.getElementById("projectsImg");
+    var contactImg = document.getElementById("contactImg");
+    member.style.backgroundColor = "#f5f5f5";
+    skills.style.backgroundColor = "#fff";
+    projects.style.backgroundColor = "#fff";
+    contact.style.backgroundColor = "#fff";
+    memberText.style.color = "#000";
+    skillsText.style.color = "#000";
+    projectsText.style.color = "#000";
+    contactText.style.color = "#000";
+    memberImg.src = "img/member.png";
+    skillsImg.src = "img/skills.png";
+    projectsImg.src = "img/projects.png";
+    contactImg.src = "img/contact.png";
+}
